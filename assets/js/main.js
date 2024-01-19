d628006e7939967d0bbf35488c58c8beed690d78
AdrianoR85 <adrianorosa085@gmail.com>
2024-01-19 11:24:06 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,11 @@
 const links =  document.querySelectorAll('.navegation__links ul li a')
 const pages = document.querySelectorAll('.page')
+const ageElem = document.querySelector('.info-item__age')
 
 function changePage(menuName) {
     pages.forEach(page => {
         const pageId = page.id
-
+        console.log(pageId)
         if(menuName === pageId) {
             addClass(page, 'show')
         } else {
@@ -31,3 +32,21 @@ links.forEach(link => {
         changePage(nameClassOfMenu)
     })
 })
+
+function age(){
+    const date = new Date()
+    const currentDay = date.getDay()
+    const currentMonth = date.getMonth() + 1
+    const currentYear = date.getFullYear()
+    let myAge = ''
+
+    if(currentDay === 31 && currentMonth === 1 ) {
+        myAge = currentYear - 1985
+    } else {
+        myAge = currentYear - 1985 - 1
+    }
+
+    ageElem.textContent = myAge
+}
+
+age()
